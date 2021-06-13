44d7674fab85bf340dd0f859a8ccaa671ced4901
Marina Monsivais <minamonsi@me.com>
2021-06-12 22:18:49 -0500

    Completed custom hook

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from '../hooks/useForm';
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,7 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const [values, handleChanges] = useForm(initialValue);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -87,7 +84,7 @@ const CheckoutForm = (props) => {
         </div>
       )}
     </>
-  );
-};
+  ); 
+}
 
 export default CheckoutForm;
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initValue) => {
+    const [values, setValues] = useState(initValue);
+
+    const handleChanges = (e) => {
+        setValues({ 
+            ...values, 
+            [e.target.name]: e.target.value 
+        });
+      };
+
+      const clearForm = e => {
+          e.preventDefault();
+          setValues(initValue);
+      }
+
+      return [values, setValues, handleChanges, clearForm]
+}
